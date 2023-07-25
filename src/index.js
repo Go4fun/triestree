59b4c9422046962e62732646a4ed613cc20d818b
suxiang <9cengta@sina.com>
2023-07-25 12:50:12 +0800

    将coo.js作为一个独立的文件，里面构建了TrieNode和Trie。Trie提供了 insert, insertFromFile, search, filter(过滤掉指定词并替换成*),startsWith等方法

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ console.log('it is good');
 // 导入coo.js
 const Trie = require('./coo');
 
-tree = new Trie();
-console.log(tree)
+// 创建一个 Trie 实例
+const trie = new Trie();
+
+// 向 Trie 中插入单词.
+//其实这里就是建立一个敏感词过滤字典
+//这个字典是用trie结构来构建的
+trie.insert("apple");
+trie.insert("banana");
+trie.insert("cat");
+trie.insert("dog");
+
+// 搜索单词
+console.log(trie.search("apple")); // 输出: true
+console.log(trie.search("banana")); // 输出: true
+console.log(trie.search("cat")); // 输出: true
+console.log(trie.search("dog")); // 输出: true
+console.log(trie.search("car")); // 输出: false
+console.log(trie.search("ban")); // 输出: false
+console.log(trie.search("apple1")); // 输出: false
+console.log("~~~~~~~~~~~~~~~~"); // 输出分隔符
+
+// 搜索前缀
+console.log(trie.startsWith("app")); // 输出: true
+console.log(trie.startsWith("ban")); // 输出: true
+console.log(trie.startsWith("c")); // 输出: true
+console.log(trie.startsWith("d")); // 输出: true
+console.log(trie.startsWith("ca")); // 输出: true
+console.log(trie.startsWith("de")); // 输出: false
+console.log(trie.startsWith("ana")); // 输出: false
 
 
